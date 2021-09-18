b6efec5cf4824978e2e958f040ab281626e890d5
ryangamble <15951175+ryangamble@users.noreply.github.com>
2021-09-17 21:50:47 -0400

    Update user_routes.py

--- trendr/routes/user_routes.py
+++ trendr/routes/user_routes.py
@@ -1,17 +1,6 @@
-import json
-from flask import request, Blueprint
-
-from trendr.controllers import user_controller
-from trendr.routes.helpers.json_response import json_response
+from flask import Blueprint
 
 users = Blueprint('users', __name__)
-
-
-@users.route('/', methods=['POST'])
-def create_user():
-    request_data = json.loads(request.data)
-    user = user_controller.create_user(request_data["name"], request_data["password"])
-    return json_response(user)
 
 
 @users.route('/', methods=['GET'])
@@ -24,11 +13,11 @@ def get_users_by_id(user_id):
     pass
 
 
-@users.route('/<user_id', methods=['GET'])
+@users.route('/<user_id', methods=['PUT'])
 def update_user(user_id):
     pass
 
 
-@users.route('/<user_id>', methods=['GET'])
+@users.route('/<user_id>', methods=['DELETE'])
 def delete_user(user_id):
     pass
