d91cc93d5e6ea07d35e5505cedbc186a6cf6baf5
kevinpan47 <kpan8055@gmail.com>
2021-09-21 17:27:50 -0400

    fixed aspect ratio of charts

--- react-frontend/src/components/Results/PriceChart.js
+++ react-frontend/src/components/Results/PriceChart.js
@@ -1,11 +1,13 @@
 import React from "react";
 import {Container} from "react-bootstrap";
 import TradingViewWidget, {Themes, BarStyles} from "react-tradingview-widget";
+import "./Results.css";
 
 // Currently pass symbol as a prop, can be changed later
 function PriceChart(props) {
   return(
-    <Container fluid>
+    <Container fluid >
+      <div className="chartContainer">
       <TradingViewWidget
         symbol={props.symbol}
         theme={Themes.LIGHT}
@@ -14,7 +16,9 @@ function PriceChart(props) {
         interval="D"
         locale="en"
         enable_publishing={false}
+        autosize={true}
       />
+      </div>
     </Container>
   );
 }
