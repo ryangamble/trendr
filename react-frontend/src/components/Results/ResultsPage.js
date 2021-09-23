e64ffff137a52d761403ae0b6e3688ffccf1b61f
kevinpan47 <kpan8055@gmail.com>
2021-09-23 16:10:33 -0400

    updated styling

--- react-frontend/src/components/Results/ResultsPage.js
+++ react-frontend/src/components/Results/ResultsPage.js
@@ -2,15 +2,26 @@ import React from "react";
 import MyNavBar from "../NavBar/MyNavBar";
 import PriceChart from "./PriceChart";
 import Statistics from "./Statistics";
+import {Container, Col, Row} from "react-bootstrap";
 
-function Home() {
+function Results() {
   return (
-    <div>
-      <MyNavBar />
-      <PriceChart symbol="AAPL"/>
-      <Statistics />
+    <div className="resultsPage">
+      <MyNavBar/>
+      <Container>
+        <Row>
+        <Col xs={12} sm={12} md={12} lg={6}>
+          <PriceChart symbol="NOVA"/>
+          <br/>
+        </Col>
+        <Col xs={12} sm={12} md={12} lg={6}>
+          <Statistics symbol="NOVA"/>
+          <br/>
+        </Col>
+        </Row>
+      </Container>
     </div>
   );
 }
 
-export default Home;
+export default Results;
--- react-frontend/src/components/Results/Statistics.js
+++ react-frontend/src/components/Results/Statistics.js
@@ -1,65 +1,84 @@
 import React, { useEffect, useState } from "react";
-import {Container} from "react-bootstrap";
+import {Container, Image, ListGroup, Row, Col} from "react-bootstrap";
 import axios from "axios";
 import "./Results.css"
 
-function Statistics() {
+function Statistics(props) {
   
   const [stock, setStock] = useState([]);
 
   const requestBody = {
     method: 'POST',
     headers: { 'Content-Type': 'application/json'},
-    name: 'AAPL'
+    name: props.symbol
   }
 
   useEffect(() => {
     axios.post('/api/result/stats', requestBody)
       .then(res => {
-        const response = JSON.parse(JSON.stringify(res.data));
+        return JSON.parse(JSON.stringify(res.data));
+      })
+      .then (data => {
         setStock({
-          companyName: response['longName'],
-          logo: response['logo_url'],
-          symbol: response['symbol'],
-          openPrice: '$' +response['open'],
-          dayHigh: '$' + response['dayHigh'],
-          dayLow: '$' + response['dayLow'],
-          fiftyTwoWeekHigh: '$' + response['fiftyTwoWeekHigh'],
-          fiftyTwoWeekLow: '$' + response['fiftyTwoWeekLow'],
-          volume: response['volume'],
-          avgVolume: response['averageVolume'],
-          marketCap: '$' + getNumberUnit(response['marketCap']),
-          pegRatio: (response['pegRatio']).toFixed(2),
-          divYield: (response['dividendYield'] * 100).toFixed(2)
+          companyName: data['longName'] ? data['longName'] : data['shortName'],
+          logo: data['logo_url'],
+          symbol: data['symbol'],
+          dayOpen: data['open'].toFixed(2),
+          dayHigh: data['dayHigh'].toFixed(2),
+          dayLow: data['dayLow'].toFixed(2),
+          fiftyTwoWeekHigh: data['fiftyTwoWeekHigh'].toFixed(2),
+          fiftyTwoWeekLow: data['fiftyTwoWeekLow'].toFixed(2),
+          volume: data['volume'],
+          avgVolume: data['averageVolume'],
+          marketCap: getNumberUnit(data['marketCap']),
+          pegRatio: data['pegRatio'] ? (data['pegRatio']).toFixed(2) : "N/A",
+          divYield: data['dividendYield'] ? (data['dividendYield'] * 100).toFixed(2) : "N/A"
         });
 
-        console.log(response);
+        console.log(data);
+      })
+      .catch((error) => {
+        console.log(error);
       });
-      
       
   }, []);
 
   function getNumberUnit (num) {
-    var units = ["M","B","T"]
+    var units = ["M", "B", "T", "Q"]
     var unit = Math.floor((num / 1.0e+1).toFixed(0).toString().length)
     var r = unit % 3
-    var x =  Math.abs(Number(num))/Number('1.0e+'+(unit-r)).toFixed(2)
+    var x =  Math.abs(Number(num)) / Number('1.0e+' + (unit - r)).toFixed(2)
     return x.toFixed(2) + units[Math.floor(unit / 3) - 2]
   }
 
   return (
     <Container fluid>
-      <div className="generalInfoContainer">
-        {stock.companyName}
-        <br/>
-        {stock.symbol}
-        <br/>
-        {stock.pegRatio}
-        <br/>
-        {stock.divYield}
-        <br/>
-        {stock.marketCap}
-      </div>
+      <Row>
+        <Image src={stock.logo} rounded/>
+        <h2>{stock.companyName}</h2>
+      </Row>
+      <Row>
+        <Col>
+        <ListGroup variant="flush">
+          <ListGroup.Item>Symbol: {stock.symbol}</ListGroup.Item>
+          <ListGroup.Item>Day Open: {stock.dayOpen}</ListGroup.Item>
+          <ListGroup.Item>Day High: {stock.dayHigh}</ListGroup.Item>
+          <ListGroup.Item>Day Low: {stock.dayLow}</ListGroup.Item>
+          <ListGroup.Item>52 Week High: {stock.fiftyTwoWeekHigh}</ListGroup.Item>
+          <ListGroup.Item>52 Week Low: {stock.fiftyTwoWeekLow}</ListGroup.Item>
+        </ListGroup>
+        </Col>
+        <Col>
+        <ListGroup variant="flush">
+          <ListGroup.Item>Volume: {stock.volume}</ListGroup.Item>
+          <ListGroup.Item>Average Volume: {stock.avgVolume}</ListGroup.Item>
+          <ListGroup.Item>52 Week High: {stock.fiftyTwoWeekHigh}</ListGroup.Item>
+          <ListGroup.Item>Div/Yield: {stock.divYield}</ListGroup.Item>
+          <ListGroup.Item>PEG ratio: {stock.pegRatio}</ListGroup.Item>
+          <ListGroup.Item>Market Cap: {stock.marketCap}</ListGroup.Item>
+        </ListGroup>
+        </Col>
+      </Row>
     </Container>
   );
 }
