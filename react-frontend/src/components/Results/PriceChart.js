19495bc4b5ce25ffc0d7f1fe006d6932c7bcf2de
kevinpan47 <kpan8055@gmail.com>
2021-09-23 23:21:44 -0400

    first iteration of custom price chart

--- react-frontend/src/components/Results/PriceChart.js
+++ react-frontend/src/components/Results/PriceChart.js
@@ -1,26 +1,122 @@
-import React from "react";
-import {Container} from "react-bootstrap";
-import TradingViewWidget, {Themes, BarStyles} from "react-tradingview-widget";
+import React, {useEffect, useState} from "react";
+import {Container, Spinner, Row, Col} from "react-bootstrap";
+import {
+  XAxis,
+  YAxis,
+  HorizontalGridLines,
+  FlexibleXYPlot,
+  LineSeries,
+  Highlight,
+  VerticalGridLines
+} from "react-vis";
+import axios from "axios";
 import "./Results.css";
+import '../../../node_modules/react-vis/dist/style.css';
 
 // Currently pass symbol as a prop, can be changed later
 function PriceChart(props) {
-  return(
-    <Container fluid>
-      <div className="chartContainer">
-      <TradingViewWidget
-        symbol={props.symbol}
-        theme={Themes.LIGHT}
-        style={BarStyles.AREA}
-        save_image={false}
-        interval="D"
-        locale="en"
-        enable_publishing={false}
-        autosize={true}
-      />
-      </div>
-    </Container>
-  );
+
+  const [priceData, setPriceData] = useState([]);
+  const [lastLocation, setLastLocation] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  const requestBody = {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json'},
+    name: "MSFT"
+  }
+
+  useEffect(() => {
+    setLoading(true);
+    axios.post('/api/result/history', requestBody)
+    .then(res => {
+      return JSON.parse(JSON.stringify(res.data));
+    })
+    .then(data => {
+      var pd = [];
+      for (var key in data['Close']) {
+        // console.log(key)
+        // console.log(data['Close'][key].toFixed(2))
+        pd.push({
+          x: new Date(key * 1000),
+          y: data['Close'][key].toFixed(2)
+        });
+      }
+      return pd;
+    })
+    .then(pd => {
+      setPriceData(pd);
+      console.log(pd);
+    })
+    .then(() => {
+      setLoading(false);
+    })
+  }, []);
+
+  if (loading) {
+    return (
+      <Container fluid>
+        <Spinner animation="border"/>  
+      </Container>
+    );
+  } else {
+    return(
+      <Container fluid>
+        <Row>
+          <div>
+            <h2>Price chart:</h2>
+          </div>
+        </Row>
+        <Row>
+          <div className="chartContainer">
+            <FlexibleXYPlot
+              animation
+              xType="time"
+              
+              xDomain={
+                lastLocation && [
+                  lastLocation.left,
+                  lastLocation.right
+                ]
+              }
+              yDomain={
+                lastLocation && [
+                  lastLocation.bottom,
+                  lastLocation.top
+                ]
+              }
+              height={300}
+              width={400}
+              >
+              <HorizontalGridLines />
+              <VerticalGridLines />
+
+              <YAxis title="price"/>
+              <XAxis title="time"/>
+
+              <LineSeries
+                data={priceData}
+                strokeWidth={2}
+                opacity={1}
+                color="#0D6EFD"
+              />
+              <Highlight
+                onBrushEnd={area => setLastLocation(area)}
+                onDrag={area => {
+                  setLastLocation({
+                    bottom: lastLocation.bottom + (area.top - area.bottom),
+                    left: lastLocation.left - (area.right - area.left),
+                    right: lastLocation.right - (area.right - area.left),
+                    top: lastLocation.top + (area.top - area.bottom)
+                  });
+                }}
+              />
+            </FlexibleXYPlot>
+          </div>
+        </Row>
+      </Container>
+    );
+  }
 }
 
 export default PriceChart;
--- trendr/routes/result_routes.py
+++ trendr/routes/result_routes.py
@@ -3,11 +3,20 @@ import yfinance as yf
 
 result = Blueprint('result', __name__, url_prefix="/api/result")
 
-@result.route('/stats', methods=['GET', 'POST'])
-def general():
+@result.route('/stats', methods=['POST'])
+def stats():
     content = request.get_json()
 
     print("\nfetching general stock stats for: " + content['name'] + "\n")
     
     stock = yf.Ticker(content['name'])
     return jsonify(stock.info)
+
+@result.route('/history', methods=['POST'])
+def history():
+    content = request.get_json()
+    
+    print("\nfetching history market data for: " + content['name'] + "\n")
+
+    stock = yf.Ticker(content['name'])
+    return stock.history(period="1y", interval="1d").to_json()
