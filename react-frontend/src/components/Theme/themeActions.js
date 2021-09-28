b77f8cdd6f6954d1283021b61e9465d60ec3381a
Cindy1229 <leticiashi3@gmail.com>
2021-09-28 16:14:36 -0400

    added persisted state through local storage

--- react-frontend/src/components/Theme/themeActions.js
+++ react-frontend/src/components/Theme/themeActions.js
@@ -20,6 +20,28 @@ export const themes = {
   },
 };
 
+function saveToLocalStorage(store) {
+  try {
+    const serializedStore = JSON.stringify(store);
+    window.localStorage.setItem("store", serializedStore);
+  } catch (e) {
+    console.log(e);
+  }
+}
+
+function loadFromLocalStorage() {
+  try {
+    const serializedStore = window.localStorage.getItem("store");
+    if (serializedStore === null) return undefined;
+    return JSON.parse(serializedStore);
+  } catch (e) {
+    console.log(e);
+    // return undefined;
+    window.localStorage.setItem("store", JSON.stringify(initialState));
+    return initialState;
+  }
+}
+
 const initialState = {
   currentTheme: themes.light,
 };
@@ -55,8 +77,12 @@ const reducer = (state = initialState, action) => {
   }
 };
 
-export const store = createStore(reducer);
+const persistedState = loadFromLocalStorage();
+
+export const store = createStore(reducer, persistedState);
 
 console.log("initial state", store.getState());
 
 store.subscribe(() => console.log("updated state", store.getState()));
+
+store.subscribe(() => saveToLocalStorage(store.getState()));
