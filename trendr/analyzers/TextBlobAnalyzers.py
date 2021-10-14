446b8163fe217bd3783cdb4ceda567d9026e5380
kevinpan47 <kpan8055@gmail.com>
2021-10-13 20:29:35 -0400

    added textblob testing functions

---
+++ trendr/analyzers/TextBlobAnalyzers.py
@@ -0,0 +1,20 @@
+from textblob import TextBlob
+from textblob import Blobber
+from textblob.sentiments import NaiveBayesAnalyzer
+from nltk.sentiment import SentimentIntensityAnalyzer
+import nltk
+import pandas as pd
+
+nltk.download('vader_lexicon')
+
+def pattern_analyzer(text):
+  tb = TextBlob(text)
+  return tb.sentiment
+
+def naive_bayes(text):
+  tb = Blobber(analyzer=NaiveBayesAnalyzer())
+  return tb(text).sentiment
+
+def vader(text):
+  sia = SentimentIntensityAnalyzer()
+  return sia.polarity_scores(text)
