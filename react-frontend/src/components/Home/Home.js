8e753668a73ec3ab4a535853769edd858207c2b4
ryangamble <rgamble@arxan.com>
2021-10-29 17:33:18 -0400

    Revert merge

--- react-frontend/src/components/Home/Home.js
+++ react-frontend/src/components/Home/Home.js
@@ -3,16 +3,14 @@ import MyNavBar from "../NavBar/MyNavBar";
 import SearchBar from "../SearchBar/SearchBar";
 
 import { useSelector } from "react-redux";
-import FearGreed from "../FearGreed/FearGreed";
 
 function Home() {
   const currentTheme = useSelector((state) => state.theme.currentTheme);
 
   return (
-    <div style={{ backgroundColor: currentTheme.background, height: "100vh" }}>
+    <div style={{ backgroundColor: currentTheme.background }}>
       <MyNavBar />
       <SearchBar />
-      <FearGreed />
     </div>
   );
 }
