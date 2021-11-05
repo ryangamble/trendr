572c1d6717b4596c2a16fc4b975b2e3e283a2ba2
bhtechno <salaali@purdue.edu>
2021-11-05 04:31:00 +0000

    Add Aggregator to Celery

--- trendr/analyzers/posts_weight.py
+++ trendr/analyzers/posts_weight.py
@@ -1,9 +1,9 @@
 import sys
-from trendr.models import tweet_model
-from trendr.models import reddit_model
 from datetime import datetime
 import pandas as pd
 import numpy as np
+from trendr.models.reddit_model import RedditComment, RedditSubmission, Subreddit
+from trendr.models.tweet_model import Tweet
 
 hours_time_delta_scores = {
     0:10, 1:9, 2:8, 3:8, 4:7, 5:7, 6:7, 7:7, 8:7, 9:7, 10:6, 11:6, 12:6, 13:6, 14:6
@@ -11,7 +11,6 @@ hours_time_delta_scores = {
 
 for i in range(14, 25):
     hours_time_delta_scores[i] = 6
-
 
 def time_delta_hours(date: datetime) -> int:
     """
@@ -39,7 +38,7 @@ def time_delta_days(date: datetime) -> int:
     days  = duration.days
     return days
 
-def tweet_score(tweet: tweet_model.Tweet(), old_score: bool=False) -> float:
+def tweet_score(tweet: Tweet, old_score: bool=False) -> float:
     time_score = 5
     time_delta = time_delta_hours(tweet.tweeted_at)
     if old_score:
@@ -126,10 +125,7 @@ def tweet_score(tweet: tweet_model.Tweet(), old_score: bool=False) -> float:
     return score
 
 
-
-
-
-def get_tweets_moving_average(days: int, tweetsList:[tweet_model.Tweet()]) -> [float]:
+def get_tweets_moving_average(days: int, tweetsList:[Tweet]) -> [float]:
     if len(tweetsList) < 1:
         return [0]
     elif len(tweetsList) == 1:
@@ -166,13 +162,13 @@ def get_tweets_moving_average(days: int, tweetsList:[tweet_model.Tweet()]) -> [f
         elif np.isnan(movingAvg[i]):
             avg = (movingAvg[i - 1] + movingAvg[i + 1]) / 2
             movingAvg[i] = avg
+        if np.isnan(movingAvg[i]):
+            movingAvg[i] = mean
 
     return movingAvg
 
 
-
-
-def reddit_post_score(post: reddit_model.RedditSubmission(), old_score: bool=False) -> float:
+def reddit_post_score(post: RedditSubmission, old_score: bool=False) -> float:
     time_score = 5
     time_delta = time_delta_hours(post.posted_at)
     if old_score:
@@ -208,7 +204,7 @@ def reddit_post_score(post: reddit_model.RedditSubmission(), old_score: bool=Fal
     score = (comments_count * 3 + subscribers_score) * post.sentiment_score * post.score * time_score
     return score
 
-def get_reddit_post_moving_average(days: int, posts:[reddit_model.RedditSubmission()]) -> [float]:
+def get_reddit_post_moving_average(days: int, posts:[RedditSubmission]) -> [float]:
     if len(posts) < 1:
         return [0]
     elif len(posts) == 1:
@@ -237,6 +233,7 @@ def get_reddit_post_moving_average(days: int, posts:[reddit_model.RedditSubmissi
 
     #calculate moving average
     movingAvg = pd.Series(daysAvg).rolling(window=days).mean().iloc[days-1:].values
+
     for i in range(len(movingAvg)):
         if i == 0 and np.isnan(movingAvg[i]):
             movingAvg[0] = movingAvg[1]
@@ -245,33 +242,116 @@ def get_reddit_post_moving_average(days: int, posts:[reddit_model.RedditSubmissi
         elif np.isnan(movingAvg[i]):
             avg = (movingAvg[i - 1] + movingAvg[i + 1]) / 2
             movingAvg[i] = avg
-    # mean = np.nanmean(movingAvg, axis=0)
-    # #Find indices that you need to replace
-    # inds = np.where(np.isnan(a))
-    # #Place column means in the indices. A
+        if np.isnan(movingAvg[i]):
+            mean = np.nanmean(movingAvg, axis=0)
+            movingAvg[i] = mean
+
     return movingAvg
 
 
-# post = reddit_model.RedditSubmission()
-# post.subreddit = reddit_model.Subreddit()
-# post.posted_at = datetime(2021, 11, 2, 10, 55, 59, 342380)
-# post.subreddit.subscribers = 100
-# post.comments = [reddit_model.RedditComment(), reddit_model.RedditComment(), reddit_model.RedditComment()]
-# post.sentiment_score = 1.0
-# post.score = 0.6
+def reddit_comment_score(post: RedditComment, old_score: bool=False) -> float:
+    time_score = 5
+    time_delta = time_delta_hours(post.posted_at)
+    if old_score:
+        time_score = 6
+    elif time_delta > 120:
+        time_score = 2
+    elif time_delta > 96:
+        time_score = 3
+    elif time_delta > 24:
+        time_score = 4
+    else:
+        time_score = hours_time_delta_scores[time_delta]
+
+    subscribers_score = 1
+    if post.subreddit.subscribers > 1000000:
+        subscribers_score = 10
+    elif post.subreddit.subscribers > 500000:
+        subscribers_score = 8
+    elif post.subreddit.subscribers > 100000:
+        subscribers_score = 7
+    elif post.subreddit.subscribers > 30000:
+        subscribers_score = 6
+    elif post.subreddit.subscribers > 10000:
+        subscribers_score = 5
+    elif post.subreddit.subscribers > 5000:
+        subscribers_score = 3
+    elif post.subreddit.subscribers > 1000:
+        subscribers_score = 2
+    else:
+        subscribers_score = 1
+
+    score = subscribers_score * post.sentiment_score * post.score * time_score
+    return score
+
+def get_reddit_comment_moving_average(days: int, posts:[RedditComment]) -> [float]:
+    if len(posts) < 1:
+        return [0]
+    elif len(posts) == 1:
+        return [reddit_comment_score(posts[0])]
+
+    maxDays = 0
+    for post in posts:
+        time_delta = time_delta_days(post.posted_at)
+        if time_delta > maxDays:
+            maxDays = time_delta
+
+    postDaysDict = {}
+    for i in range(maxDays + 1):
+        postDaysDict[i] = []
+
+    for post in posts:
+        time_delta = time_delta_days(post.posted_at)
+        if time_delta < 0:
+            continue
+        postDaysDict[time_delta].append(reddit_comment_score(post))
+
+    daysAvg = []
+    for day in postDaysDict:
+        avg = np.average(postDaysDict[day])
+        daysAvg.append(avg)
+
+    #calculate moving average
+    movingAvg = pd.Series(daysAvg).rolling(window=days).mean().iloc[days-1:].values
+
+    for i in range(len(movingAvg)):
+        if i == 0 and np.isnan(movingAvg[i]):
+            movingAvg[0] = movingAvg[1]
+        elif i == len(movingAvg) - 1 and np.isnan(movingAvg[i]):
+            movingAvg[i] = movingAvg[i - 1]
+        elif np.isnan(movingAvg[i]):
+            avg = (movingAvg[i - 1] + movingAvg[i + 1]) / 2
+            movingAvg[i] = avg
+        if np.isnan(movingAvg[i]):
+            mean = np.nanmean(movingAvg, axis=0)
+            movingAvg[i] = mean
+
+    return movingAvg
 
 
-# post2 = reddit_model.RedditSubmission()
-# post2.subreddit = reddit_model.Subreddit()
-# post2.posted_at = datetime(2021, 10, 2, 10, 55, 59, 342380)
-# post2.subreddit.subscribers = 1000
-# post2.comments = [reddit_model.RedditComment(), reddit_model.RedditComment(), reddit_model.RedditComment()]
-# post2.sentiment_score = -0.5
-# post2.score = 0.9
 
 
-# print('scores = ', reddit_post_score(post))
-# print(get_reddit_post_moving_average(1, [post, post2]))
+
+post = RedditComment()
+post.subreddit = Subreddit()
+post.posted_at = datetime(2021, 11, 2, 10, 55, 59, 342380)
+post.subreddit.subscribers = 100
+# post.comments = [RedditComment(), RedditComment(), RedditComment()]
+post.sentiment_score = 1.0
+post.score = 0.6
+
+
+post2 = RedditComment()
+post2.subreddit = Subreddit()
+post2.posted_at = datetime(2021, 10, 2, 10, 55, 59, 342380)
+post2.subreddit.subscribers = 1000
+# post2.comments = [RedditComment(),RedditComment(), RedditComment()]
+post2.sentiment_score = -0.5
+post2.score = 0.9
+
+
+print('scores = ', reddit_comment_score(post))
+print(get_reddit_comment_moving_average(1, [post, post2]))
 
 
 # print('poest type = ', type(post))
