4632e3dc37ef4ae01e2eee996234f6df0a0bc323
kevinpan47 <kpan8055@gmail.com>
2021-12-06 11:39:48 -0500

    removed populate db request from frontend

--- react-frontend/src/components/Home/Home.js
+++ react-frontend/src/components/Home/Home.js
@@ -9,15 +9,6 @@ import axios from 'axios'
 function Home () {
   const currentTheme = useSelector((state) => state.theme.currentTheme)
 
-  useEffect(() => {
-    axios.post('http://localhost:5000/assets/populate_assets')
-      .then(res => {
-        console.log(res.data);
-      }).catch(err => {
-        console.log(err);
-      });
-  }, []);
-
   return (
     <div style={{ backgroundColor: currentTheme.background, height: '100vh' }}>
       <MyNavBar />
